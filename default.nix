908945d06c448d2c5dfe124ff356d6449331b245
InternetUnexplorer <internetunexplorer@gmail.com>
2020-12-23 22:35:42 -0800

    openrgb: remove

--- default.nix
+++ default.nix
@@ -17,6 +17,5 @@ in {
   capitaine-cursors = callPackageSuper ./capitaine-cursors { };
   inconsolata = callPackageSuper ./inconsolata { };
   multimc = callPackageSuper ./multimc { };
-  openrgb = callPackageSuper ./openrgb { };
   plasma5 = super.plasma5 // { kwin = callPackageSuper ./kwin { }; };
 }
