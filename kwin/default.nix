76dda9b3078a2fb007e8197dd071528a23205d08
InternetUnexplorer <internetunexplorer@gmail.com>
2020-12-23 22:32:46 -0800

    kwin: kwin-lowlatency: 5.18.5-2 -> 5.20.4

--- kwin/default.nix
+++ kwin/default.nix
@@ -1,13 +1,13 @@
 { plasma5, fetchpatch }:
 
-let llPatchVersion = "5.18.5-2";
+let llPatchVersion = "5.20.4";
 
 in plasma5.kwin.overrideAttrs (old: {
   patches = (old.patches or [ ]) ++ [
     (fetchpatch {
       url =
-        "https://tildearrow.zapto.org/storage/kwin-lowlatency/kwin-lowlatency-${llPatchVersion}.patch";
-      sha256 = "009g7kay8lq593x3f1qi8j43nmmlkg9xkhvzqvyn90066k2f89qb";
+        "https://github.com/tildearrow/kwin-lowlatency/releases/download/v${llPatchVersion}/kwin-lowlatency-${llPatchVersion}.patch";
+      sha256 = "0w5bqhgpzmcnpby9rlki1m3705y47cvlq0yl976605bv7s5bj39l";
     })
   ];
 })
