1c4ccaec8ab985eff782b98e87b909da82031a99
InternetUnexplorer <internetunexplorer@gmail.com>
2021-02-03 19:10:33 -0800

    reStream: 1.0 -> 1.1

--- reStream/default.nix
+++ reStream/default.nix
@@ -3,13 +3,13 @@
 
 stdenv.mkDerivation rec {
   pname = "reStream";
-  version = "1.0";
+  version = "1.1";
 
   src = fetchFromGitHub {
     owner = "rien";
     repo = "reStream";
     rev = version;
-    sha256 = "0k6pm7v0hym2423r40yp1nd7zm06qmnzwf490ab04y0nr9mz2fw7";
+    sha256 = "18z17chl7r5dg12xmr3f9gbgv97nslm8nijigd03iysaj6dhymp3";
   };
 
   nativeBuildInputs = [ makeWrapper ];
