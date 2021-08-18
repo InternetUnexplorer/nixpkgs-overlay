66864e7f6159db93a388c75cabf30897a6e7b2ba
InternetUnexplorer <internetunexplorer@gmail.com>
2021-08-17 17:21:45 -0700

    plasma-applet-thermal-monitor: init at 1.3.0

---
+++ plasma-applet-thermal-monitor/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, lib, fetchFromGitLab, fetchpatch, extra-cmake-modules, plasma5Packages
+}:
+
+stdenv.mkDerivation rec {
+  pname = "plasma-applet-thermal-monitor";
+  version = "1.3.0";
+
+  src = fetchFromGitLab {
+    owner = "agurenko";
+    repo = pname;
+    rev = version;
+    hash = "sha256-M8Z8wKyxqLHaX+8ZbzjOKkY4a7cH0ZMVhqYkZyuXXTc=";
+  };
+
+  nativeBuildInputs = [ extra-cmake-modules plasma5Packages.wrapQtAppsHook ];
+
+  buildInputs = [ plasma5Packages.plasma-workspace ];
+
+  meta = with lib; {
+    description =
+      "Plasma 5 applet for monitoring CPU, GPU and other available temperature sensors";
+    inherit (src.meta) homepage;
+    license = licenses.gpl2;
+    platforms = platforms.linux;
+  };
+}
