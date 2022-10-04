eaa44e20150a1b57bae2c5485edf29a07356c663
InternetUnexplorer <internetunexplorer@gmail.com>
2022-10-04 02:00:01 -0700

    gh-actions: don't try to build broken packages

--- build_matrix.py
+++ build_matrix.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import json
+from dataclasses import dataclass
+from os import environ
 from subprocess import check_output, run, DEVNULL
 from typing import Dict, List
-import json
-
 
 BINARY_CACHE_URLS = [
     "https://internetunexplorer.cachix.org",
@@ -11,51 +12,69 @@ BINARY_CACHE_URLS = [
 ]
 
 
-def get_packages() -> Dict[str, str]:
+@dataclass
+class Package:
+    path: str
+    broken: bool
+
+    def is_in_cache(self, cache: str) -> bool:
+        command = ["nix", "path-info", "--store", cache, self.path]
+        return run(command, stdout=DEVNULL, stderr=DEVNULL).returncode == 0
+
+    def is_in_any_cache(self) -> bool:
+        return any(self.is_in_cache(cache) for cache in BINARY_CACHE_URLS)
+
+    def should_be_built(self) -> bool:
+        return not self.broken and not self.is_in_any_cache()
+
+
+def get_packages() -> Dict[str, Package]:
     output = check_output(
         [
             "nix",
             "eval",
+            "--impure",
             "--recreate-lock-file",
             "--json",
             ".#packages.x86_64-linux",
-        ]
+            "--apply",
+            """
+            builtins.mapAttrs (_: drv: {
+                path = drv.outPath;
+                broken = drv.meta.broken;
+            })
+            """,
+        ],
+        env=dict(environ, NIXPKGS_ALLOW_BROKEN="1"),
     )
-    return json.loads(output)
+
+    return {name: Package(**fields) for name, fields in json.loads(output).items()}
 
 
-def is_path_in_cache(path: str, cache_url: str) -> bool:
-    result = run(
-        ["nix", "path-info", "--store", cache_url, path], stdout=DEVNULL, stderr=DEVNULL
-    )
-    return result.returncode == 0
-
-
-def is_path_in_any_cache(path: str) -> bool:
-    return any(map(lambda c: is_path_in_cache(path, c), BINARY_CACHE_URLS))
-
-
-def get_packages_to_build(packages: Dict[str, str]) -> List[str]:
-    return [name for name, path in packages.items() if not is_path_in_any_cache(path)]
+def print_packages(title: str, packages: List[Package]) -> None:
+    BLUE, RESET = "\033[34m", "\033[0m"
+    print(BLUE + f"---- {title} ".ljust(60, "-") + RESET)
+    for package in packages or [None]:
+        print(f"  {package.path if package else '(none)'}")
+    print()
 
 
 if __name__ == "__main__":
+    print()
+
     packages = get_packages()
+    print_packages("Packages in flake", list(packages.values()))
 
-    BLUE, RESET = "\033[34m", "\033[0m"
+    broken_packages = [name for name, package in packages.items() if package.broken]
+    print_packages(
+        "Packages marked as broken", [packages[name] for name in broken_packages]
+    )
 
-    print()
-    print(f"{BLUE}---- Packages in flake -------------------------------------{RESET}")
-    for path in packages.values():
-        print(" ", path)
-    print()
-
-    packages_to_build = get_packages_to_build(packages)
-    print(f"{BLUE}---- Packages to be built ----------------------------------{RESET}")
-    for path in [packages[name] for name in packages_to_build]:
-        print(" ", path)
-    if not packages_to_build:
-        print("  (none)")
-    print()
+    packages_to_build = [
+        name for name, package in packages.items() if package.should_be_built()
+    ]
+    print_packages(
+        "Packages to be built", [packages[name] for name in packages_to_build]
+    )
 
     print("::set-output", f"name=packages::{json.dumps(packages_to_build)}")
