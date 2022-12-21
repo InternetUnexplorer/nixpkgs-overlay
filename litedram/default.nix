2f15610632a963401157207ba4278eb7c0e80e5b
InternetUnexplorer <internetunexplorer@gmail.com>
2022-12-20 19:08:55 -0800

    litedram: init at 2022.08 (WIP)

---
+++ litedram/default.nix
@@ -0,0 +1,33 @@
+{ lib, fetchFromGitHub, python3Packages }:
+
+python3Packages.buildPythonApplication rec {
+  pname = "litedram";
+  version = "2022.08";
+
+  src = fetchFromGitHub {
+    owner = "enjoy-digital";
+    repo = "litedram";
+    rev = version;
+    hash = "sha256-ihwEvVWlPqIIfPrnmoQsGU/utHo3QA68J85rS+tSMLE=";
+  };
+
+  propagatedBuildInputs = with python3Packages; [
+    (callPackage ./litex.nix { })
+    migen
+    pyyaml
+  ];
+
+  doCheck = false; # FIXME
+
+  passthru = {
+    autoUpdate = "git-tags";
+    exePath = "/bin/litedram_gen";
+  };
+
+  meta = with lib; {
+    description = "Small footprint and configurable DRAM core";
+    inherit (src.meta) homepage;
+    license = licenses.bsd2;
+    platforms = platforms.unix;
+  };
+}
---
+++ litedram/litex.nix
@@ -0,0 +1,26 @@
+{ lib, fetchFromGitHub, buildPythonApplication, migen, pyserial, requests }:
+
+buildPythonApplication rec {
+  pname = "litex";
+  version = "2022.08";
+
+  src = fetchFromGitHub {
+    owner = "enjoy-digital";
+    repo = "litex";
+    rev = version;
+    hash = "sha256-ibiUzNckxPK//mPrjPIpzOzo0D+FIhUomXeIiXdenhc=";
+  };
+
+  propagatedBuildInputs = [ migen pyserial requests ];
+
+  doCheck = false; # FIXME
+
+  passthru.autoUpdate = "git-tags";
+
+  meta = with lib; {
+    description = "A Migen/MiSoC based Core/SoC builder";
+    inherit (src.meta) homepage;
+    license = licenses.bsd2;
+    platforms = platforms.unix;
+  };
+}
