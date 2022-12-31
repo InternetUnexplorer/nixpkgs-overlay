e3ca69d07d76f8dbbbb92af6c7c8f76d695a96a5
InternetUnexplorer <internetunexplorer@gmail.com>
2022-12-31 02:29:34 -0800

    esp-rust-bin: hopefully fix compilation?

--- esp-rust-bin/default.nix
+++ esp-rust-bin/default.nix
@@ -1,24 +1,57 @@
-{ stdenv, lib, fetchurl, symlinkJoin, callPackage }:
+{ stdenv, lib, fetchurl, fetchzip, autoPatchelfHook, gcc, zlib, darwin }:
 
 # NOTE: This is a work-in-progress
 
 let
   version = "1.66.0.0";
+
   repository = "https://github.com/esp-rs/rust-build";
+
+  downloadUrl = "${repository}/releases/download/v${version}";
+
   platform = {
     aarch64-darwin = "aarch64-apple-darwin";
     aarch64-linux = "aarch64-unknown-linux-gnu";
     x86_64-darwin = "x86_64-apple-darwin";
     x86_64-linux = "x86_64-unknown-linux-gnu";
   }.${stdenv.system};
-in symlinkJoin rec {
-  name = "esp-rust-bin-${version}";
+
+in stdenv.mkDerivation rec {
+  pname = "esp-rust-bin";
+
+  inherit version;
 
   src = fetchurl {
-    url = let filename = "rust-${version}-${platform}.tar.xz";
-    in "${repository}/releases/download/v${version}/${filename}";
-    hash = (builtins.fromJSON (builtins.readFile ./checksums.json)).${platform};
+    url = "${downloadUrl}/rust-${version}-${platform}.tar.xz";
+    hash = {
+      aarch64-apple-darwin =
+        "sha256-nJMLowwf7Mz9o/+fDa+xA6c6L3wexu/MsLKPnI/OhBE=";
+      aarch64-unknown-linux-gnu =
+        "sha256-yEMoTcP4uXDINlzMVlqUvJmQP8tH/2IfvYkAUWhpAjc=";
+      x86_64-apple-darwin =
+        "sha256-MJOw+Si9AJwolfVVFHC6i42JIti4h2vskxuo2kYi9RM=";
+      x86_64-unknown-linux-gnu =
+        "sha256-jgSVwYYcQB8SAJR1j/Qhzmn8fcqumZT615qTSfHifVE=";
+    }.${platform};
   };
+
+  rust-src = fetchzip {
+    url = "${downloadUrl}/rust-src-${version}.tar.xz";
+    hash = "sha256-JZl6w/oey/b8Efp6kodv8CifSy7snwFIUvUIIp9eD0U=";
+  };
+
+  nativeBuildInputs = lib.optional (!stdenv.isDarwin) autoPatchelfHook;
+  buildInputs = lib.optionals (!stdenv.isDarwin) [ gcc.cc.lib zlib ]
+    ++ lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
+
+  installPhase = ''
+    runHook preInstall
+    bash install.sh --destdir=$out --prefix="" \
+      --components=rustc,rust-std-${platform},cargo
+    bash ${rust-src}/install.sh --destdir=$out --prefix="" \
+      --components=rust-src
+    runHook postInstall
+  '';
 
   meta = with lib; {
     description =
@@ -28,9 +61,4 @@ in symlinkJoin rec {
     platforms =
       [ "aarch64-darwin" "aarch64-linux" "x86-64-darwin" "x86_64-linux" ];
   };
-
-  rustc = callPackage ./rustc.nix { inherit src version meta platform; };
-  cargo = callPackage ./cargo.nix { inherit src version meta rustc; };
-
-  paths = [ rustc cargo ];
 }
