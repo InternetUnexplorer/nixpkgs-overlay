8da62d9f338cf3cdb76429c3d8a0bc24321c5496
InternetUnexplorer <internetunexplorer@gmail.com>
2023-01-07 18:58:14 -0800

    cargo-espflash: 2.0.0-rc.1 -> 2.0.0-rc.2

--- cargo-espflash/default.nix
+++ cargo-espflash/default.nix
@@ -1,22 +1,26 @@
-{ cargo-espflash, fetchFromGitHub }:
+{ cargo-espflash, fetchFromGitHub, openssl, udev }:
 
 cargo-espflash.overrideAttrs (old: rec {
-  version = "2.0.0-rc.1";
+  version = "2.0.0-rc.2";
 
   src = fetchFromGitHub {
     owner = "esp-rs";
     repo = "espflash";
     rev = "v${version}";
-    hash = "sha256-7SZtgJoiFikOgR/PYof14cnaYQmQ0LKIUC2tuEUpUOk=";
+    hash = "sha256-g20dhsWWf5T2Hcr9HEkezwwx1Q/ZQkN0ch/zEH6MeII=";
   };
+
+  buildInputs = [ openssl udev ];
+
+  OPENSSL_NO_VENDOR = 1;
 
   # This will fail, because update.py doesn't know how to update this.
   # The failure will trigger a notification, so this is basically a reminder to
   # update it manually.
-  passthru.autoUpdate = "github-releases";
+  passthru.autoUpdate = "git-tags";
 
   cargoDeps = old.cargoDeps.overrideAttrs (_: {
     inherit src;
-    outputHash = "sha256-J8BZEmYxFCupc2fOXuk1svY213iB4/lmzpDLAcdcpZI=";
+    outputHash = "sha256-Mh3ryJV8ae53PKRHTC3EVd15ZjGDVvbQKSzgXEVDGOc=";
   });
 })
