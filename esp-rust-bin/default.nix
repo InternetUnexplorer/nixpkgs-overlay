48438b3b1b80439ee7d00457eeac3925a71293a8
InternetUnexplorer <internetunexplorer@gmail.com>
2023-01-28 18:24:31 -0800

    esp-rust-bin: 1.66.0.0 -> 1.67.0.0

--- esp-rust-bin/default.nix
+++ esp-rust-bin/default.nix
@@ -3,7 +3,7 @@
 # NOTE: This is a work-in-progress
 
 let
-  version = "1.66.0.0";
+  version = "1.67.0.0";
 
   repository = "https://github.com/esp-rs/rust-build";
 
@@ -25,19 +25,20 @@ in stdenv.mkDerivation rec {
     url = "${downloadUrl}/rust-${version}-${platform}.tar.xz";
     hash = {
       aarch64-apple-darwin =
-        "sha256-nJMLowwf7Mz9o/+fDa+xA6c6L3wexu/MsLKPnI/OhBE=";
+        "sha256-2c4CXvPuMAQ7xe2pJKIIMELSYSCltZrFx/Nrm6VpM6c=";
       aarch64-unknown-linux-gnu =
-        "sha256-yEMoTcP4uXDINlzMVlqUvJmQP8tH/2IfvYkAUWhpAjc=";
+        "sha256-95i8LYnk4pX+KStEHP2NZeHNzACykn53743+9X1Q+lc=";
       x86_64-apple-darwin =
-        "sha256-MJOw+Si9AJwolfVVFHC6i42JIti4h2vskxuo2kYi9RM=";
+        "sha256-s/VVefoc7Q1c79f1jzCu9G0bm+Wcb+R3h9qP14+qEAI=";
       x86_64-unknown-linux-gnu =
-        "sha256-jgSVwYYcQB8SAJR1j/Qhzmn8fcqumZT615qTSfHifVE=";
+        "sha256-Tk1GQvzlXcMfIkN+yXtDA2BIkK00iKwLGt6eWNlKGsM=";
     }.${platform};
   };
 
   rust-src = fetchzip {
+    name = "rust-src-${version}";
     url = "${downloadUrl}/rust-src-${version}.tar.xz";
-    hash = "sha256-JZl6w/oey/b8Efp6kodv8CifSy7snwFIUvUIIp9eD0U=";
+    hash = "sha256-fFDPRA8iNh6vjnSHyBt+ymmlkOUbZEoFcBoDXyEfBog=";
   };
 
   nativeBuildInputs = lib.optional (!stdenv.isDarwin) autoPatchelfHook;
