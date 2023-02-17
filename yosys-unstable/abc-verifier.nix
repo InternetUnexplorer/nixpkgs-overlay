387d351870d12a5a93daa9ce07ef794735558fce
InternetUnexplorer <internetunexplorer@gmail.com>
2023-02-16 21:53:04 -0800

    yosys-unstable: init at unstable-2023-02-17

---
+++ yosys-unstable/abc-verifier.nix
@@ -0,0 +1,20 @@
+{ abc-verifier, fetchFromGitHub }:
+
+abc-verifier.overrideAttrs (final: prev: {
+  version = "unstable-2023-02-04";
+
+  src = fetchFromGitHub {
+    owner = "YosysHQ";
+    repo = "abc";
+    rev = "a8f0ef2368aa56b3ad20a52298a02e63b2a93e2d";
+    hash = "sha256-48i6AKQhMG5hcnkS0vejOy1PqFbeb6FpU7Yx0rEvHDI=";
+  };
+
+  passthru = prev.passthru // {
+    inherit (final.src) rev;
+    autoUpdate = "git-commits";
+  };
+
+  pos.file = ./default.nix; # TODO: this seems like a hack :(
+  pos.line = 1;
+})
---
+++ yosys-unstable/default.nix
@@ -0,0 +1,23 @@
+{ yosys, callPackage, fetchFromGitHub }:
+
+let abc-verifier = callPackage ../abc-verifier { };
+
+in (yosys.override { inherit abc-verifier; }).overrideAttrs (final: prev: {
+  version = "unstable-2023-02-17";
+
+  src = fetchFromGitHub {
+    owner = "YosysHQ";
+    repo = "yosys";
+    rev = "1cfedc90ce7b2bd63d75883a4e8c36fd44f0e4e7";
+    hash = "sha256-t9X7Vj+TnK3gKyNE1MD65dNzIRMdD65Hmq6YWA68GWo=";
+  };
+
+  postPatch = (prev.postPatch or "") + ''
+    sed 's/YOSYS_VER := .*/YOSYS_VER := ${final.version}/g' -i Makefile
+  '';
+
+  passthru = prev.passthru // { autoUpdate = "git-commits"; };
+
+  pos.file = ./default.nix; # TODO: this seems like a hack :(
+  pos.line = 1;
+})
