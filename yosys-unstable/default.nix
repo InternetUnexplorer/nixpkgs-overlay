5e02da8260dcc70d941eebda61d32b292f31ef35
InternetUnexplorer <internetunexplorer@gmail.com>
2023-06-27 02:28:00 -0700

    yosys-unstable: add updateScript

--- yosys-unstable/default.nix
+++ yosys-unstable/default.nix
@@ -1,4 +1,4 @@
-{ yosys, callPackage, fetchFromGitHub }:
+{ yosys, callPackage, fetchFromGitHub, writeShellScript, nix-update }:
 
 let abc-verifier = callPackage ./abc-verifier.nix { };
 
@@ -12,9 +12,20 @@ in (yosys.override { inherit abc-verifier; }).overrideAttrs (final: prev: {
     hash = "sha256-V6VfWwsWLkceiV4Ch87ZUB/SiNVspOfdX+bQ8zqKmVY=";
   };
 
+  inherit abc-verifier;
+
   postPatch = (prev.postPatch or "") + ''
     sed 's/YOSYS_VER := .*/YOSYS_VER := ${final.version}/g' -i Makefile
   '';
 
   patches = [ ./plugin-search-dirs.patch ];
+
+  passthru.updateScript = writeShellScript "update-yosys-unstable" ''
+    set -e -u -o pipefail
+    ${nix-update}/bin/nix-update --flake yosys-unstable --version branch
+    YOSYS_SRC=$(nix build .#yosys-unstable.src --no-link --print-out-paths)
+    ABCREV=$(awk '/^ABCREV/ {print $NF}' $YOSYS_SRC/Makefile)
+    ${nix-update}/bin/nix-update --flake yosys-unstable.abc-verifier --version "$ABCREV"
+  '';
+  passthru.exePath = "/bin/yosys";
 })
