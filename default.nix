755c9875301ed113b1037ec27a91c2651dd2aa42
InternetUnexplorer <internetunexplorer@gmail.com>
2023-07-08 18:14:32 -0700

    flake: fix stack overflow when using overlay

--- default.nix
+++ default.nix
@@ -1,1 +1,6 @@
-final: prev: import ./all-packages.nix { inherit (prev) lib callPackage; }
+final: prev:
+
+import ./all-packages.nix {
+  inherit (prev) lib;
+  callPackage = prev.lib.callPackageWith prev;
+}
