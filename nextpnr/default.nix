569d229e6d2c3aff59bdbb5daa33eb0795fc1469
InternetUnexplorer <internetunexplorer@gmail.com>
2025-01-24 06:38:06 -0800

    nextpnr: 0.7 -> unstable-2025-01-23

--- nextpnr/default.nix
+++ nextpnr/default.nix
@@ -1,4 +1,4 @@
-{ nextpnrWithGui, fetchpatch }:
+{ nextpnrWithGui, fetchFromGitHub, lib, fetchpatch }:
 
 let
   fix-gowin-gui = fetchpatch {
@@ -6,5 +6,15 @@ let
     in "https://github.com/YosysHQ/nextpnr/commit/${commit}.patch";
     sha256 = "sha256-LSQ+1tDf8L47W+s9T0iT8iGU/Yu6xdO7bji94fBEHA0=";
   };
-in nextpnrWithGui.overrideAttrs
-(old: { patches = (old.patches or [ ]) ++ [ fix-gowin-gui ]; })
+in nextpnrWithGui.overrideAttrs (old: {
+  version = "unstable-2025-01-23";
+
+  src = fetchFromGitHub {
+    owner = "YosysHQ";
+    repo = "nextpnr";
+    rev = "0c060512c1bf6719391e2d3351c8cb757bec29cc";
+    sha256 = "sha256-BLNu84riTL/Wg6y/XuxKjvq5G4EUY5gVC4t717yzSgg=";
+  };
+
+  patches = (old.patches or [ ]) ++ [ fix-gowin-gui ];
+})
